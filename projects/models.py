58d1e611bb708930bc8b69dc5993a14cb9337475
plo <kostasarto2@gmail.com>
2023-10-24 09:32:32 +0300

    feat: create project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    collaborators = models.ManyToManyField(User, related_name="collaborator_set")
+
+    name = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=255, null=True)
+    maximum_collaborators = models.IntegerField()
