ab5d07fa8e28cff0e0b96523f26673151670702c
plo <kostasarto2@gmail.com>
2023-10-24 09:51:00 +0300

    fix: alter fields to add the option of being empty

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,13 @@ from users.models import User
 
 class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    collaborators = models.ManyToManyField(User, related_name="collaborator_set")
+    collaborators = models.ManyToManyField(
+        User, related_name="collaborator_set", blank=True
+    )
 
-    name = models.CharField(max_length=255, null=True)
-    description = models.CharField(max_length=255, null=True)
-    maximum_collaborators = models.IntegerField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    maximum_collaborators = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
