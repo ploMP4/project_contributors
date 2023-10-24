185485fd3fb1d8e590cd0b4edafe951a65aa183a
plo <kostasarto2@gmail.com>
2023-10-24 09:38:51 +0300

    feat: add text choices for skill level and programming language

--- users/models.py
+++ users/models.py
@@ -9,6 +9,25 @@ class User(AbstractUser):
 
 
 class Skill(models.Model):
+    class Language(models.TextChoices):
+        CPP = "C++"
+        JAVASCRIPT = "javascript"
+        PYTHON = "python"
+        JAVA = "java"
+        LUA = "lua"
+        RUST = "rust"
+        GO = "go"
+        JULIA = "julia"
+
+    class Level(models.TextChoices):
+        BEGINNER = "beginner"
+        EXPERIENCED = "experienced"
+        EXPERT = "expert"
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    language = models.CharField(max_length=255, null=True)
-    level = models.CharField(max_length=255, null=True)
+    language = models.CharField(
+        max_length=255, choices=Language.choices, default=Language.PYTHON
+    )
+    level = models.CharField(
+        max_length=255, choices=Level.choices, default=Level.BEGINNER
+    )
