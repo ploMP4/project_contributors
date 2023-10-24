08353b1ea8b8d7a671568583f657aa0e5fa74654
plo <kostasarto2@gmail.com>
2023-10-24 12:37:26 +0300

    feat: create user register view

---
+++ users/serializers.py
@@ -0,0 +1,31 @@
+from typing import Dict
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import User
+
+
+class UserSerializerWithToken(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "age",
+            "country",
+            "residence",
+            "token",
+        ]
+
+    def get_token(self, obj: User) -> Dict[str, str]:
+        refresh = RefreshToken.for_user(obj)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from rest_framework.response import Response
+from rest_framework.views import APIView, status
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializerWithToken
+
+
+class RegisterUser(APIView):
+    def post(self, request) -> Response:
+        data = request.data
+
+        user = User(
+            username=data.get("username"),
+            password=make_password(data.get("password")),
+            email=data.get("email"),
+            first_name=data.get("first_name", ""),
+            last_name=data.get("last_name", ""),
+            age=data.get("age", None),
+            country=data.get("country", ""),
+            residence=data.get("residence", ""),
+        )
+
+        try:
+            user.full_clean()
+            user.save(force_insert=True)
+
+            serializer = UserSerializerWithToken(user, many=False)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as error:
+            return Response({"message": error}, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response(
+                {"message": "User already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
