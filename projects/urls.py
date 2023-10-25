5421fc5093dd84619694845bc0cb646896b21360
plo <kostasarto2@gmail.com>
2023-10-25 16:25:24 +0300

    feat: create view to retrieve, update and delete a project

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import CreateProjectView
+from .views import CreateProjectView, RetrieveUpdateDeleteProjectView
 
 urlpatterns = [
     path("", CreateProjectView.as_view(), name="project_create"),
+    path(
+        "<int:pk>/",
+        RetrieveUpdateDeleteProjectView.as_view(),
+        name="project_get_update_delete",
+    ),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,19 @@
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
+from .models import Project
 from .serializers import ProjectSerializer
 
 
 class CreateProjectView(CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ProjectSerializer
+
+
+class RetrieveUpdateDeleteProjectView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        queryset = Project.objects.filter(id=self.kwargs["pk"], owner=self.request.user)
+        return queryset
