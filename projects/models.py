ac29f5231b1bae20c62b10455c65c25d5d2e91d5
plo <kostasarto2@gmail.com>
2023-10-25 16:23:14 +0300

    feat: add completed field to the project model

--- projects/models.py
+++ projects/models.py
@@ -11,6 +11,7 @@ class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     maximum_collaborators = models.IntegerField(default=5)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
