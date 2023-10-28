c358e89c1892f316c9b8d2f1d2f9f38118a75195
plo <kostasarto2@gmail.com>
2023-10-29 00:59:28 +0300

    feat: implement password reset views

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,30 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import CreateSkillView, DeleteSkillView, RegisterUserView
+from .views import (
+    CreateSkillView,
+    DeleteSkillView,
+    RegisterUserView,
+    RequestPasswordResetUserView,
+    ResetPasswordUserView,
+    RetrieveUserWithStatsView,
+)
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterUserView.as_view(), name="register"),
+    path(
+        "password-reset/",
+        RequestPasswordResetUserView.as_view(),
+        name="password_reset_request",
+    ),
+    path(
+        "password-reset/<int:pk>/<str:token>/",
+        ResetPasswordUserView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path("<int:pk>/stats/", RetrieveUserWithStatsView.as_view(), name="user_stats"),
     path("skill/", CreateSkillView.as_view(), name="skill"),
     path("skill/<int:pk>/", DeleteSkillView.as_view(), name="skill_delete"),
 ]
--- users/views.py
+++ users/views.py
@@ -39,6 +39,62 @@ class RegisterUserView(CreateAPIView):
     serializer_class = UserSerializerWithToken
 
 
+class RequestPasswordResetUserView(APIView):
+    def post(self, request):
+        """
+        Send an email to the user containing a url with a PasswordResetToken
+        to send a patch request to with the new password
+        """
+        try:
+            user = User.objects.get(email=request.data.get("email"))
+            token = PasswordResetTokenGenerator().make_token(user)
+
+            reset_url_args = {"pk": user.id, "token": token}
+            reset_path = reverse("password_reset_confirm", kwargs=reset_url_args)
+
+            user.email_user(
+                subject="Project Contributors Password Reset",
+                message=f"Send a patch request to the following url: {reset_path}",
+                from_email=settings.EMAIL_HOST_USER,
+            )
+
+            return Response(
+                {"message": f"Email has been sent to {user.email}"},
+                status=status.HTTP_200_OK,
+            )
+        except ObjectDoesNotExist:
+            return Response(
+                {"message": "There is no user with the provided email address"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class ResetPasswordUserView(APIView):
+    def patch(self, request, pk, token):
+        """
+        Validate the PasswordResetToken and set the new password
+        provided by the user
+        """
+        try:
+            user = User.objects.get(id=pk)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise ValidationError("token is not valid")
+
+            user.set_password(request.data.get("password"))
+            user.clean()
+            user.save()
+
+            return Response(
+                {"message": "Password reset successfully"},
+                status=status.HTTP_200_OK,
+            )
+        except ValidationError as e:
+            return Response(
+                {"message": e.detail},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+
 class CreateSkillView(CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = SkillSerializer
