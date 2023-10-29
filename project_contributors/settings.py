2f6668e4b6acfd3c9ed952cff28bc2f582637142
ploMP4 <kostasarto2@gmail.com>
2023-10-29 20:00:11 +0200

    chore: make access token last longer

--- project_contributors/settings.py
+++ project_contributors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,6 +134,10 @@ STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
+}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
